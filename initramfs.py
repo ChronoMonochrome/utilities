3fb8ca4396738ac0def075ebadd2c9a286278893
Chrono Monochrome <chrono.monochrome@gmail.com>
2014-10-06 12:38:09 +0400

    Messages style

--- initramfs.py
+++ initramfs.py
@@ -6,8 +6,10 @@ DEBUG = 1
 GZIP_MAGIC="\x1F\x8B\x08"
 LZ4_LEGACY_MAGIC='\x02\x21\x4c\x18'
 CPIO_MAGIC='070701'
+CPIO_END_SIG='TRAILER'
 GZIP = 'gzip'
 LZ4 = 'lz4'
+UNKNOWN='?'
 
 def debug_print(s):
 	global DEBUG
@@ -33,6 +35,8 @@ def find_zimage_start(s):
 		      s.find(LZ4_LEGACY_MAGIC)+1), LZ4 
 	elif is_gziped(s):
 		res = s.find(GZIP_MAGIC), GZIP
+	else:
+		return -1, UNKNOWN
 	return res
 
 def extract_cpio():
@@ -40,9 +44,8 @@ def extract_cpio():
 	
 	try:
 		bootimg = open("./"+sys.argv[1],'rb').read()
-		debug_print('%s was successfully readed'%sys.argv[1])
 	except:
-		debug_print('fail during read boot.img')
+		debug_print('error occured when reading boot.img')
 		return -1
 		
 	ZIMAGE_START, FORMAT = find_zimage_start(bootimg)
@@ -59,21 +62,27 @@ def extract_cpio():
 			return -2
 		try:		
 			if FORMAT == LZ4:
-				os.system("lz4c -d ./kernel.lz4 ./kernel")
+				os.system("lz4c -dy ./kernel.lz4 ./kernel")
 			if FORMAT == GZIP:
 				os.system("gunzip -qf kernel.gz")
 			kernel = open('./kernel','rb').read()
-			debug_print('successfully unpacked %sd zImage' % FORMAT)
+			debug_print('%sd zImage was successfully unpacked' % FORMAT)
 		except:
 			debug_print('error occured when unpacking zImage')
 			return -3
 	else:
-		debug_print('zImage was not found')
+		debug_print('zImage is not found')
 		return -4
 	CPIO_START = kernel.find(CPIO_MAGIC)
+	CPIO_END = len(kernel)
+	#CPIO_START = len(kernel)-kernel[::-1].find(CPIO_MAGIC[::-1], kernel[::-1].find(CPIO_MAGIC[::-1])+1)
+	#debug_print('cpio_start=%d'%CPIO_START)
+	#CPIO_END = len(kernel) - kernel[::-1].find(CPIO_END_SIG[::-1]),
+		   #   kernel[::-1].find(CPIO_END_SIG[::-1])+1)
+	debug_print('cpio_end=%d'%CPIO_END)
 	if (CPIO_START >= 0): 
 		debug_print('found ramdisk at %d'%CPIO_START)
-		open('./initramfs.cpio','wb').write(kernel[CPIO_START:])
+		open('./initramfs.cpio','wb').write(kernel[CPIO_START:CPIO_END])
 	else:
 		debug_print('error occured when writing ramdisk')
 		return -5
